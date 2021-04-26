05100d1b2ba7a140550f2a106b9daa3a8a983fbf
stairsofhenry <78277370+stairsofhenry@users.noreply.github.com>
2021-04-26 15:12:29 -0400

    Create index.sql

---
+++ index.sql
@@ -0,0 +1,2 @@
+CREATE TABLE info (date TEXT, language TEXT, lines_of_code INTEGER, other TEXT, id INTEGER PRIMARY KEY);
+INSERT INTO info (date, language, lines_of_code, other) VALUES ("mon, Apr 26, 2021", "SQL", 2, "it's this");
